29792118729eb708339a0bae7b67e95da5073484
NikitaTsmakalov <nikita4343ttg@gmail.com>
2025-05-19 01:34:54 +0300

    Реализовано BottomBar для приложения

---
+++ app/src/main/java/com/example/jetgym/Activity/MainActivity/MainContent.kt
@@ -0,0 +1,11 @@
+package com.example.jetgym.Activity.MainActivity
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.jetgym.Model.Workout
+
+@Composable
+fun MainContent(
+    modifier: Modifier=Modifier,
+    workouts:List<Workout>
+){}
